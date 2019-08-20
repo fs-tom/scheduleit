68e2a2d011cbf57026131e6276b559be283bd9a0
tom <tom@the-cloud>
2019-08-20 17:14:22 +0000

    Branching into clojure version

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to scheduleit
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/scheduleit/core_test.clj
@@ -0,0 +1,7 @@
+(ns scheduleit.core-test
+  (:require [clojure.test :refer :all]
+            [scheduleit.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
