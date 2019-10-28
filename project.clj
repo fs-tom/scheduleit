63649ef554db467ff5651ca956f76385bd07ce87
tom <tom@that-place>
2019-10-28 09:34:18 -0500

    Squeaking progress

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [spork "0.2.1.1-SNAPSHOT"]]
-  :repl-options {:init-ns scheduleit.core})
+  ;:repl-options {:init-ns scheduleit.core}
+  )
